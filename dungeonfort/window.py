48d01c04e1d0c7260587fd77b95cda9ba21f28bb
Minh Pham <minh.pham@utexas.edu>
2016-10-30 00:17:05 -0500

    Renamed main source directory to project name.

---
+++ dungeonfort/window.py
@@ -0,0 +1,12 @@
+try:
+  from OpenGL.GLUT import *
+  from OpenGL.GL import *
+  from OpenGL.GLU import *
+except:
+  print('ERROR: PyOpenGL not installed properly.')
+
+class Window:
+  def __init__():
+    # foo
+
+
